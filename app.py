d503a27f02bfbbae8495d5aee7f8ab75897044f1
Andrea Diaz <andrea96silvald@gmail.com>
2024-10-26 10:17:25 -0500

    config. streamlit

--- app.py
+++ app.py
@@ -15,3 +15,11 @@ if hist_button:  # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    ...

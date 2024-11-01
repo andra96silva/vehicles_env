6c5b3ba1bbcfcc2e28d182dfed46155af01a27fb
Andrea Diaz <andrea96silvald@gmail.com>
2024-11-01 07:24:38 -0500

    incorporación header

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv')  
-hist_button = st.button('Construir histograma') 
+st.header('vehicles_env')
 
-if hist_button:  
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir histograma')
 
-    
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
     fig = px.histogram(car_data, x="odometer")
 
-    
     st.plotly_chart(fig, use_container_width=True)
-
 
 
 build_histogram = st.checkbox('Construir un histograma')
 
-if build_histogram:  
+if build_histogram:
     st.write('Construir un histograma para la columna odómetro')
     ...
